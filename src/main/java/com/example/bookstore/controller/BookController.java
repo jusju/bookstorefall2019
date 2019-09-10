0c54c24078164da39ac0c3daefb341bc1e821bf4
Juslin Jukka <jusju@haaga-helia.fi>
2019-09-10 14:21:05 +0300

    first commit

---
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class BookController {
+	@RequestMapping("/index")
+	public String index() {
+		return "Hello Bookstore";
+	}
+}
